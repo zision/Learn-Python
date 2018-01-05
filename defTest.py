69b6c0f6f257d56ae5bb3ede4b2334e33e328007
zision <zhouzijian1996@gmail.com>
2018-01-06 01:24:26 +0800

    学习PEP8风格编码，学习函数

--- defTest.py
+++ defTest.py
@@ -1,9 +1,39 @@
-#尝试一般的加法定义
-def jiafa(a,b):
+# 尝试一般的加法定义
+def jia_fa(a,b):
     return a + b
-print(jiafa(3,4))
 
-#尝试默认减2的减法定义
-def jianfa(a,b=2):
+
+print(jia_fa(3, 4))
+
+# 尝试默认减2的减法定义
+
+
+def jian_fa(a, b=2):
+    # 定义一个加法
     return a - b
-print(jianfa(4))
+
+
+print(jian_fa(4))
+
+# 元组类型的变长度参数传递
+
+
+def da_yin1(a, *b):
+    print(a, b)
+
+
+da_yin1(1, 2, 3, 4)
+
+# 试试没有括号的
+
+
+def da_yin2(a, *b):
+    print(a, end='')
+    print(' ', end='')
+    for i in b:
+        print(i, end='')
+        print(' ', end='')
+
+
+da_yin2(1, 2, 3, 4, 5, 6, 7, 8, 9)
+

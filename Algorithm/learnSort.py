944da2276fe64368d8cf6761c85e8f9f4ccdef67
zision <zhouzijian1996@gmail.com>
2018-03-27 22:56:11 +0800

    增加直接插入排序

--- Algorithm/learnSort.py
+++ Algorithm/learnSort.py
@@ -7,14 +7,14 @@ from functools import wraps
 
 
 # 计时装饰器
-def fn_timer(function):
-    @wraps(function)
+def fn_timer(fn):
+    @wraps(fn)
     def function_timer(*args, **kwargs):
         t0 = time.clock()
-        result = function(*args, **kwargs)
+        result = fn(*args, **kwargs)
         t1 = time.clock()
         print ("函数'%s'本次运行耗时: %s 秒" %
-               (function.func_name, str(t1-t0))
+               (fn.func_name, str(t1-t0))
                )
         return result
     return function_timer
@@ -25,19 +25,30 @@ def get_random(num):
     a = []
     i = 0
     while i < num:
-        a.append(random.randint(0, 1000))
+        a.append(random.randint(0, 100))
         i += 1
     return a
 
 
+# 直接插入排序
+@fn_timer
+def insert_sort(array):
+    for i in range(len(array)):
+        for j in range(i):
+            if array[i] < array[j]:
+                array.insert(j, array.pop(i))
+                break
+    return array
+
+
 # 冒泡排序
 @fn_timer
-def bubble_sort(buli):
-    for i in range(len(buli)-1):
-        for j in range(len(buli)-i-1):
-            if buli[j] > buli[j+1]:
-                buli[j], buli[j+1] = buli[j+1], buli[j]
-    return buli
+def bubble_sort(array):
+    for i in range(len(array)-1):
+        for j in range(len(array)-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
 
 
 # 输出测试
@@ -45,5 +56,5 @@ if __name__ == '__main__':
     list1 = get_random(int(input('输入随机生成列表长度:')))
     print('原列表为:%s' % list1)
     print('冒泡排序后:%s' % bubble_sort(list1))
-
+    print('直接插入排序后:%s' % insert_sort(list1))
 

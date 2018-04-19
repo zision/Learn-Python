fe6ed74b4573e53d9bbd8127bf60973bf1538db1
zision <zhouzijian1996@gmail.com>
2018-04-20 05:47:33 +0800

    学习正则表达式

--- regtest.py
+++ regtest.py
@@ -74,7 +74,7 @@ line = "XXX出生于2001年6月1日"
 # line = "XXX出生于2001/06/01"
 # line = "XXX出生于2001/06"
 
-regex_str = ".*出生于(\d{4}[年/-]\d{1,2}([月/-]$|[月/-]\d{1,2}$|[月/-]\d{1,2}日$|$))"
+regex_str = ".*出生于([1900-2050]{4}[年/-]\d{1,2}([月/-]$|[月/-]\d{1,2}$|[月/-]\d{1,2}日$|$))"
 match_obj = re.match(regex_str, line)
 if match_obj:
     print(match_obj.group(1))

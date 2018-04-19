f785fc95e48c35e540f741cc452a0105fb1c8a3b
zision <zhouzijian1996@gmail.com>
2018-04-20 05:25:51 +0800

    学习正则表达式

--- regtest.py
+++ regtest.py
@@ -64,3 +64,17 @@ regex_str = ".*?(\d+)年.*"   # \d代表数字 使用非贪婪或{4}限定提取
 match_obj = re.match(regex_str, line)
 if match_obj:
     print(match_obj.group(1), '出生年份提取成功!')
+
+
+# 提取出生日期实例
+line = "XXX出生于2001年6月1日"
+# line = "XXX出生于2001/6/1"
+# line = "XXX出生于2001-6-1"
+# line = "XXX出生于2001-06-01"
+# line = "XXX出生于2001/06/01"
+# line = "XXX出生于2001/06"
+
+regex_str = ".*出生于(\d{4}[年/-]\d{1,2}([月/-]$|[月/-]\d{1,2}$|[月/-]\d{1,2}日$|$))"
+match_obj = re.match(regex_str, line)
+if match_obj:
+    print(match_obj.group(1))
